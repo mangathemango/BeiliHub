bd28b6e719e8ebb75d2936fe04350f563b1368eb
Inkt13 <138111203+Inkt13@users.noreply.github.com>
2025-09-02 22:25:02 +0800

    description collapsing

--- js/lesson.js
+++ js/lesson.js
@@ -1,6 +1,9 @@
 const sideBar = document.getElementById('toggleSidebar');
 const videoList = document.getElementById('videoList');
-const arrow = document.getElementById('arrow')
+const arrow = document.getElementById('arrow');
+const desc = document.getElementById('video-desc');
+const descBody = document.getElementById('video-desc-body');
+const descArrow= document.getElementById('downArrow');
 /*const arrow = toggle.querySelector('.arrow');*/
 
 sideBar.addEventListener('click', () => {
@@ -10,4 +13,10 @@ sideBar.addEventListener('click', () => {
         }
     })
     arrow.classList.toggle('flipped')
+})
+
+desc.addEventListener('click', () => {
+    console.log("NIGGER")
+    descBody.classList.toggle('video-desc-body-collapsed')
+    descArrow.classList.toggle('flippedVertical')
 })
